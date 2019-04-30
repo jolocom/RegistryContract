7d12b68642edceb5427671a7ceb91419c44833b1
VolkerSchiewe <volker.s1994@gmail.com>
2019-04-30 11:31:25 +0200

    use latest solidity, add recovery options to smart contract

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,10 +1,11 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.7;
 
 contract Registry {
 
     struct Record {
         address owner;
         string ddoHash;
+        address recovery;
     }
 
     mapping (bytes32 => Record) private didToHash;
@@ -15,16 +16,53 @@ contract Registry {
         owner = msg.sender;
     }
 
-    function setRecord(bytes32 did, string _newHash) public {
-        bytes memory emptyTest = bytes(didToHash[did].ddoHash); 
+    function setRecord(bytes32 did, string memory newHash) public {
+        bytes memory emptyTest = bytes(didToHash[did].ddoHash);
         if (emptyTest.length != 0 && didToHash[did].owner != msg.sender) {
-            revert("DID registration failed. Invalid did private key.");
+            revert("DID registration failed. Invalid DID private key.");
+        }
+        address recovery = address(0);
+        if(emptyTest.length != 0){
+            recovery = didToHash[did].recovery;
         }
 
-        didToHash[did] = Record(msg.sender, _newHash);
+        didToHash[did] = Record(msg.sender, newHash, recovery);
     }
 
-    function getRecord(bytes32 _did) public view returns (string) {
-        return didToHash[_did].ddoHash;
+    function changeOwner(bytes32 did, address newOwner, string memory newHash) public {
+        if (bytes(didToHash[did].ddoHash).length == 0){
+            revert("DID is not registered");
+        }
+        if (didToHash[did].recovery != msg.sender){
+            revert("Invalid recovery private key");
+        }
+
+        didToHash[did] = Record(newOwner, newHash, address(0));
     }
+
+    function setRecovery(bytes32 did, address recoveryAddress) public {
+        bytes memory emptyTest = bytes(didToHash[did].ddoHash);
+        if (emptyTest.length == 0) {
+            revert("DID is not registered.");
+        }
+        if (emptyTest.length != 0 && didToHash[did].owner != msg.sender) {
+            revert("Invalid DID private key.");
+        }
+        didToHash[did] = Record(msg.sender, didToHash[did].ddoHash, recoveryAddress);
+    }
+
+    function getHash(bytes32 did) public view returns (string memory) {
+        return didToHash[did].ddoHash;
+    }
+
+    // TODO remove
+    function getRecoveryAddress(bytes32 _did) public view returns (address){
+        return didToHash[did].recovery;
+    }
+
+    // TODO remove
+    function getOwner(bytes32 did) public view returns(address){
+        return didToHash[did].owner;
+    }
+
 }
